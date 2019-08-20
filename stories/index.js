e1ac4c81d4b57ffc8fac8a4009eb400f3df65247
N <noel.mcloughlin@gmail.com>
2019-08-20 19:10:08 +0100

    add static filter-controls component (story 2)

--- stories/index.js
+++ stories/index.js
@@ -3,9 +3,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import Header from '../src/components/header/'
 
+import Header from '../src/components/header/'
 storiesOf("Noun App/Header", module).add("default", () => (
   <Header noPersons={10} noPlaces={10} noThings={10} />
 ));
 
+import FilterControls from '../src/components/filterControls/'
+storiesOf("Noun App/Filter Controls", module).add("default", () => (
+  <FilterControls />
+));
